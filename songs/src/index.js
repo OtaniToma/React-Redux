4d925f17334080f22e9f7b17992da0d62b66ae13
John Smith <otani.toma@gmail.com>
2019-04-25 18:39:39 -0700

    finish seasons2

--- songs/src/index.js
+++ songs/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import App from './compornents/App';
+import App from './components/App';
 import reducers from './reducers';
 
 ReactDOM.render(
