44bc2eb06fd6827d7f959a773e9287d38f0457e3
John Smith <otani.toma@gmail.com>
2019-04-27 14:07:03 -0700

    modify pics2

---
+++ pics2/src/api/unsplash.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'https://api.unsplash.com',
+  headers: {
+    Authorization: 'Client-ID e39bebfab4192b093b0271a62b50aa4166da005e8afb3ee3d983218f794f3aa0'
+  }
+});
