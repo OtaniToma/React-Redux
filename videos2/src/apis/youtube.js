2b9faf708c31bd5035e040c0333af934cc75caf5
John Smith <otani.toma@gmail.com>
2019-05-04 14:59:43 -0700

    create videos2

---
+++ videos2/src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyC-wjTiniNEukusUzoYq1jUqvmfzyfWmY8';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults:5,
+    key: KEY
+  }
+});
