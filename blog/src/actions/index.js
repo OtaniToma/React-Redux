fd5610771727720f611f54d5cf6cc924c3f81f79
John Smith <otani.toma@gmail.com>
2019-05-31 19:07:04 -0700

    add reducer

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,10 +1,14 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = () => {
-  const promise = jsonPlaceholder.get('/posts');
-   
-  return {
-    type: 'FETCH_POSTS',
-    payload: promise
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+    dispatch({ type: 'FETCH_POSTS', payload: response})
   };
-};
+
+
+// This aciton creator is no more necessary.
+// export const selectPost = () => {
+//   return {
+//     type: 'SELECT_POST'
+//   }
+// };
